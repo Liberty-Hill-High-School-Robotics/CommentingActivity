eb26851045a8f40d43f1ff9d7f8ccd7f238fd522
LeonardoHazembuller <75856125+LeonardoHazembuller@users.noreply.github.com>
2023-12-11 15:37:12 -0600

    Update Main.java

--- Base Codes/Example2/src/main/java/frc/robot/Main.java
+++ Base Codes/Example2/src/main/java/frc/robot/Main.java
@@ -10,7 +10,7 @@ public final class Main {
   }
 
   
-  public static void main(String. args) (
-    RobotBase.startRobot(Robot::new):
+  public static void main(String... args) {
+    RobotBase.startRobot(Robot::new);
   }
-)
+}
